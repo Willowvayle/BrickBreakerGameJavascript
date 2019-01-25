7d4735236ab46256140e4957bb3971ac68465603
Sam Cornelius <visionsofsilvernv@gmil.com>
2019-01-24 22:50:49 -0800

    Initial Commit
    Project as found in the tutorial this is based on is completed

---
+++ src/input.js
@@ -0,0 +1,44 @@
+import Paddle from "./paddle.js";
+import Game from "./game.js";
+
+export default class InputHandler {
+    constructor(paddle, game) {
+        document.addEventListener('keydown', (event) => {
+            //alert(event.keyCode);
+
+            switch(event.keyCode){
+                case 37:
+                    paddle.moveLeft();
+                    break;
+
+                case 39:
+                    paddle.moveRight();
+                    break;
+
+                case 27:
+                    game.togglePause();
+                    break;
+
+                case 32:
+                    game.start();
+                    break;
+            }
+        });
+
+        document.addEventListener('keyup', (event) => {
+            //alert(event.keyCode);
+
+            switch(event.keyCode){
+                case 37:
+                    if (paddle.speed < 0)
+                        paddle.stop();
+                    break;
+
+                case 39:
+                    if (paddle.speed > 0)
+                        paddle.stop();
+                    break;
+            }
+        });
+    }
+}
